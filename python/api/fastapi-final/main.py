fc95d1569e74992da495d6f72a01e29accb97dbe
freer4an <freer4an@gmail.com>
2024-10-20 00:42:49 +0500

    fastapi-final

---
+++ python/api/fastapi-final/main.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class CustomValidationError(Exception):
+
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(self.message)
+
+
+# sum1n handler body ------------------------------------------------------
+@app.get("/sum1n/{n}")
+def summorial(n: int):
+    return {"result": sum([i for i in range(1, 11)])}
+# -------------------------------------------------------------------------
+
+
+# fibonacci handler body --------------------------------------------------
+@app.get("/fibo")
+def fibonnaci(n: int):
+    if n <= 0:
+        return {"result": 0}
+    
+    prev, current = 0, 1
+    for i in range(2, n):
+        current, prev = prev + current, current
+
+    return {"result": current}
+# -------------------------------------------------------------------------
+
+
+# reverse handler body ----------------------------------------------------
+@app.post("/reverse")
+async def reverser(req: Request):
+    text = req.headers.get("string")
+    if text == None:
+        return {"result": ""}
+    return {"result": text[::-1]}
+# -------------------------------------------------------------------------
+
+
+# list handler body -------------------------------------------------------
+global_list = []
+
+class RequestList(BaseModel):
+    element: str
+
+@app.put("/list")
+async def list_handler(req: RequestList):
+    global_list.append(req.element)
+    return
+
+@app.get("/list")
+def list_handler():
+    return {"result": global_list}
+# -------------------------------------------------------------------------
+
+
+# calculator handler body -------------------------------------------------
+class RequestCalculator(BaseModel):
+    expr: str
+
+@app.post("/calculator")
+async def calculator(req: RequestCalculator):
+    try:
+        num1, operator, num2 = validateExpr(req.expr)
+        result = doMath(num1, num2, operator)
+    except CustomValidationError as err:
+        return JSONResponse(
+                status_code=err.status_code,
+                content={"error": err.message}
+            )
+    if result % 1 == 0:
+        return {"result": int(result)}
+    return {"result": result}
+
+def validateExpr(expr: str):
+    try:
+        num1, operator, num2 = expr.split(',')
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        raise CustomValidationError(status_code=400, message="invalid")
+    
+    return num1, operator, num2
+
+def doMath(num1, num2, operator):
+    result = 0
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            raise CustomValidationError(status_code=403, message="zerodiv")
+        result = num1 / num2
+    else:
+        raise CustomValidationError(status_code=400, message="invalid")
+
+    return result
+# -------------------------------------------------------------------------
